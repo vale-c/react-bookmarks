c757d5db74a1684e697d3c2cc9745a26ae84e2c3
Vale <vale@MacBook-Pro.local>
2020-04-21 10:25:38 +0200

    latest

--- development/react-bookmarks.e31bb0bc.js
+++ development/react-bookmarks.e31bb0bc.js
@@ -28549,7 +28549,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58070" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58156" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
